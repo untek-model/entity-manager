107d487beb1023f4fc7c14c2582076126f9b1625
Vitaliy <theyamshikov@yandex.ru>
2024-08-08 18:51:09 +0500

    upd

--- src/EntityManagerBundle.php
+++ src/EntityManagerBundle.php
@@ -4,13 +4,8 @@ namespace Untek\Model\EntityManager;
 
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Exception\LogicException;
-use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use Untek\Model\Cqrs\Application\Abstract\CqrsHandlerInterface;
-use Doctrine\Persistence\ObjectRepository;
 use Untek\Model\EntityManager\Attribute\AsEntityRepository;
 
 class EntityManagerBundle extends AbstractBundle
@@ -21,9 +16,6 @@ class EntityManagerBundle extends AbstractBundle
         $builder->registerAttributeForAutoconfiguration(AsEntityRepository::class, static function (ChildDefinition $definition, AsEntityRepository $attribute): void {
             $definition->addTag('repository');
         });
-
-//        $builder->registerForAutoconfiguration(ObjectRepository::class)
-//            ->addTag('repository');
 
         $container->import(__DIR__ . '/resources/config/services/entity-manager.php');
     }
