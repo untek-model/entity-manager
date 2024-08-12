7f981e43525527aa66d6cdfcd1d2edde84da4a6b
Vitaliy <theyamshikov@yandex.ru>
2024-08-12 18:01:48 +0500

    upd

--- src/EntityManagerBundle.php
+++ src/EntityManagerBundle.php
@@ -2,9 +2,11 @@
 
 namespace Untek\Model\EntityManager;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Untek\Model\EntityManager\Attribute\AsEntityRepository;
 use Untek\Model\EntityManager\DependencyInjection\EntityManagerPass;
@@ -14,8 +16,11 @@ class EntityManagerBundle extends AbstractBundle
 
     public function build(ContainerBuilder $container)
     {
-        parent::build($container);
         $container->addCompilerPass(new EntityManagerPass());
+
+        $fileLocator = new FileLocator(__DIR__);
+        $loader = new PhpFileLoader($container, $fileLocator);
+        $loader->load(__DIR__ . '/resources/config/services/entity-manager.php');
     }
 
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
@@ -23,7 +28,5 @@ class EntityManagerBundle extends AbstractBundle
         $builder->registerAttributeForAutoconfiguration(AsEntityRepository::class, static function (ChildDefinition $definition, AsEntityRepository $attribute): void {
             $definition->addTag('repository');
         });
-
-        $container->import(__DIR__ . '/resources/config/services/entity-manager.php');
     }
 }
